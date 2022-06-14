03ef266b26a101a979d461af1acf52ab8fa11ed9
Mike Griffin <mgriffin@github.com>
2022-06-14 17:15:25 +0100

    Return an image if requested

--- app/controllers/miniblobs_controller.rb
+++ app/controllers/miniblobs_controller.rb
@@ -1,5 +1,9 @@
 class MiniblobsController < ApplicationController
   def index
     @svg = Svg.new.svg
+    respond_to do |format|
+      format.html
+      format.svg { render inline: @svg, mime_type: Mime::Type.lookup("image/svg+xml") }
+    end
   end
 end
--- spec/requests/miniblobs_spec.rb
+++ spec/requests/miniblobs_spec.rb
@@ -2,9 +2,17 @@ require "rails_helper"
 
 RSpec.describe "Miniblobs", type: :request do
   describe "GET /index" do
-    it "shows an SVG" do
+    it "shows an SVG embedded in a page" do
       get root_url
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /index.svg" do
+    it "returns an SVG image" do
+      get "/miniblobs.svg"
+      expect(response).to have_http_status(:success)
+      expect(response.content_type).to match(/image\/svg\+xml/)
     end
   end
 end
