a622ca39ec577d750bcb2b8577e670a9854878ad
Mike Griffin <github@mikegriffin.ie>
2026-08-29 23:41:00 +0100

    config hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
+  config.hosts << "miniblob.mikegriffin.ie"
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
